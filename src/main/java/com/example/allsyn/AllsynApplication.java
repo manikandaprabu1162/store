fb737f7da04b888ac356464c05d98ede362a3848
manikandaprabu1162 <manikandaprabu157@gmail.com>
2026-07-01 19:52:42 +0530

    store changes

---
+++ src/main/java/com/example/allsyn/AllsynApplication.java
@@ -0,0 +1,13 @@
+package com.example.allsyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllsynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllsynApplication.class, args);
+	}
+
+}
